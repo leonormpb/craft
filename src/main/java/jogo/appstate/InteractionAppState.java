4e331dbb515a17b5c61415521fe509688bac9c8b
leonormpb <leonorbento8@gmail.com>
2025-12-08 01:27:48 +0000

    Add files via upload

--- src/main/java/jogo/appstate/InteractionAppState.java
+++ src/main/java/jogo/appstate/InteractionAppState.java
@@ -60,7 +60,7 @@ public class InteractionAppState extends BaseAppState {
         VoxelWorld vw = world != null ? world.getVoxelWorld() : null;
         if (vw != null) {
             vw.pickFirstSolid(cam, reach).ifPresent(hit -> {
-                VoxelWorld.Vector3i cell = hit.cell;  // ✅ Já está correto
+                VoxelWorld.Vector3i cell = hit.cell;
                 System.out.println("TODO (exercise): interact with voxel at " + cell.x + "," + cell.y + "," + cell.z);
             });
         }
